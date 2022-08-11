24c8ed6b12a80cb95abddc32a654da67b671b2d6
enzo <62140783+enzobo@users.noreply.github.com>
2022-08-11 10:46:02 -0300

    Update github_v2.py

--- github_v2.py
+++ github_v2.py
@@ -381,9 +381,11 @@ df, product = get_df()
 vectorizer, nbrs, clean, messy, cat_clf = clf()
 #clean, messy, cat_clf = clf()
   
-#@st.cache(allow_output_mutation=True, show_spinner=False, ttl=60*60*24)
+@st.cache(allow_output_mutation=True, show_spinner=False, ttl=60*60*24)
 def get_tst_df(df, id):
-    return df[~df.id_item.isin([int(x) for x in id.keys()])]
+    temp = df[~df.id_item.isin([int(x) for x in id.keys()])]
+    final = temp.reset_index().drop(columns='index')
+    return final
 
 ################################## Funções atribuídas aos botões ##################################
 def next_item():
